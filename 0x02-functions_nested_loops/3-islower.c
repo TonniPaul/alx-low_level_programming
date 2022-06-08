ecb72abce0822c3094c31ccd0ca10a3e751203a9
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-08 05:31:14 -0700

    modification for white space

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - Shows 1 if the input is a
- * lowercase character. Another cases, shows
- * 0
- *
- * @c: The character in ASCII code
- *
- * Return 1 for lowercase character. 0 if its not 
- */
+* _islower - Show 1 when  input is a
+* lowercase character. Another cases, shows
+* 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for lowercase character. 0 for the rest.
+*/
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
