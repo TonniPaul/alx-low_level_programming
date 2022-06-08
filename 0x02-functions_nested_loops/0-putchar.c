295d07b6170dc08a2f6744c1148e712ae482a777
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-07 18:03:46 -0700

    write a script that prints putchar without using printf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
-* Print -purchar followed by a new line
-*
+* main-print -purchar and add a new line
+*i
 * Return: always 0 (success)
 */
 int main(void)
 {
-	char z[20] = "_putchar";
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count = 0, size;
 
-	printf("%s\n", z);
+	size = sizeof(str) / sizeof(int);
+	do {
+		_putchar(str[count]);
+		count++;
+	} while (count < size);
+	_putchar('\n');
 	return (0);
 }
