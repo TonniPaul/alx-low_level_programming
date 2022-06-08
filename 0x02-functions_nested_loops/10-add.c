edb92bc7c39c3ae9b77897942dc0526825dce623
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-08 10:50:19 -0700

    this function writes the sum of in

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - adds two integers and returns the result
+*
+* @a: int 1
+* @b: int 2
+*
+* Return: sum of integers
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
