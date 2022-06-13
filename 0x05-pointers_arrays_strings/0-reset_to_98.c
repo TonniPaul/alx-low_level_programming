8a80dde1c980cc6c905abf9ac785c03cd1a2ebf7
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-13 06:06:45 -0700

    THis fuction returns the value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes a pointer to an int and updates the value..
+*
+* @n: input integer.
+*
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
