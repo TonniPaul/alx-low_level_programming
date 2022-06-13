c397f2a2eb31ce3c0c99fb2ee1e3c34bb4ee65cc
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-06-13 07:05:03 -0700

    Function prints srings in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+*
+* @s: input string.
+*
+* Return: nothing.
+*/
+void print_rev(char *s)
+{
+	int count = 0;
+
+	for (; count >= 0; count--)
+	{
+		if (s[count] == '\0')
+			break;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
