1da97381f847cc182441da3fdb72ea47ecb8cc3f
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-18 13:10:54 -0700

    Removed else

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,8 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			new->next = temp->next;
 			temp->next = new;
 			return (new);
-		} else
-			temp = temp->next;
+		}
 	}
 
 	return (NULL);
