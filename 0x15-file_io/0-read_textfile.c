22759bf45de4cc589f579a59efd848698b494f76
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-25 06:35:58 -0700

    Modified to betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	ssize_t file, fread, fwrite;
+	char *totalSize;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	totalSize = malloc(sizeof(char) * letters);
+	if (totalSize == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	fread = read(file, totalSize, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, totalSize, fread);
+	if (fwrite == -1)
+		return (0);
+	close(file);
+	free(totalSize);
+	return (fwrite);
 }
